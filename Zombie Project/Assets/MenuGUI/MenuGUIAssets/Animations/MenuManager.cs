46029fff8e77af629fc6eac41fd5f60f742f7c1d
James <50862535+JamesBridger@users.noreply.github.com>
2019-05-21 16:55:45 +0800

    MenuGUI
    MenuGUI

---
+++ Zombie Project/Assets/MenuGUI/MenuGUIAssets/Animations/MenuManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public Menu CurrentMenu;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ShowMenu(CurrentMenu);
+    }
+
+    public void ShowMenu(Menu menu)
+    {
+        if (CurrentMenu != null)
+            CurrentMenu.IsOpen = false;
+
+        CurrentMenu = menu;
+        CurrentMenu.IsOpen = true;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
