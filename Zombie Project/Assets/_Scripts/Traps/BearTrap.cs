4002e3a347d7b21b9416e332606b9059fc82aa03
TheMattster57 <43920699+TheMattster57@users.noreply.github.com>
2019-05-21 20:05:24 +0930

    Basic setup for Stationary traps
    Bear Trap - Basic code all set up
    Explosive Mine - Basic code all set up
    
    The rest needs collisions setup to continue. There is a problem with maths for explosion damage for Explosive mine

---
+++ Zombie Project/Assets/_Scripts/Traps/BearTrap.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BearTrap : MonoBehaviour
+{
+    public int uses = 3;
+
+    public float triggered = 0f;
+    public float coolDown = 2f;
+    public float damage = 20f;
+
+    public bool coolDownComplete = true;
+    public bool isDestroyed = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Destroyed();
+
+        triggered += Time.deltaTime;
+
+        if (triggered >= coolDown)
+        {
+            coolDownComplete = true;
+        }
+
+
+    }
+
+    private void Destroyed()
+    {
+        if(uses <= 0)
+        {
+            // change to destroyed sprite
+            isDestroyed = true;
+            return;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (isDestroyed == false && coolDownComplete == true)
+        {
+            // Initiate bear trap animation or sprite
+            // kill zombie
+            triggered = 0f;
+            coolDownComplete = false;
+            uses -= 1;
+        }
+    }
+
+    private void TakeHealth()
+    {
+        //targetHealth.TakeDamage(damage);
+    }
+
+    private void Repaired()
+    {
+        uses = 3;
+        isDestroyed = false;
+        coolDownComplete = true;
+        // Set sprite back to normal
+    }
+}
---
+++ Zombie Project/Assets/_Scripts/Traps/ExplosiveMine.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosiveMine : MonoBehaviour
+{
+    public bool isDestroyed = false;
+
+    public float maxDamage = 50f;
+    public float explosionRadius = 10f;
+    public float explosionForce = 80f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (isDestroyed == false)
+        {
+            // Activate explosion
+            // Deal damage
+            // Set sprite to destroyed
+            isDestroyed = true;
+        }
+        else
+        {
+            return;
+        }
+    }
+
+    private float CalculateDamage(Vector3 targetPosition)       // From the tank game to try and calculate the damge due to how close they are (might need to be recalculated for this purpose)
+    {
+        Vector3 explosionToTarget = targetPosition - transform.position;
+
+        float explosionDistance = explosionToTarget.magnitude;
+
+        float relativeDistance = (explosionRadius - explosionDistance) / explosionRadius;
+
+        float damage = relativeDistance * maxDamage;
+
+        damage = Mathf.Max(0f, damage);
+
+        return damage;
+    }
+
+    private void Repaired()
+    {
+        isDestroyed = false;
+        // Change sprite back to normal
+    }
+}
