147bd79a24a5d19ef377dfa5aca2070112b1376d
James <50862535+JamesBridger@users.noreply.github.com>
2019-05-23 19:14:59 +0800

    update
    update

--- Zombie Project/Assets/_Scripts/MousePointer/MouseCursor.cs
+++ Zombie Project/Assets/_Scripts/MousePointer/MouseCursor.cs
@@ -4,16 +4,32 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+
+    public Texture2D mainCursor;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotSpot = Vector2.zero;
+
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.visible = false;
+        //Cursor.visible = false;
     }
 
     // Update is called once per frame
-    void Update()
+    void OnGUI()
     {
-        Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        transform.position = cursorPos;
+        Ray ray2 = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit2;
+
+        if(Physics.Raycast (ray2, out hit2, 10000))
+        {
+            if(hit2.transform.tag == "ZombieProjectGUI")
+            {
+                Cursor.SetCursor(mainCursor, hotSpot, cursorMode);
+            }
+        }
+
+        //Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //transform.position = cursorPos;
     }
 }
