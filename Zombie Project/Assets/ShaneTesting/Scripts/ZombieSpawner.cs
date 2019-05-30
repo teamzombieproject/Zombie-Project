c45cea79d17a54ce22eac3b16b55ada572f08110
shild3 <50862506+shild3@users.noreply.github.com>
2019-05-30 21:15:56 +1000

    added zombie spawner
    added zombie spawner

---
+++ Zombie Project/Assets/ShaneTesting/Scripts/ZombieSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieSpawner : MonoBehaviour
+{
+
+    public GameObject zombie;
+    public float spawnTime;
+    public float spawnRepeatRate;
+    public Transform[] spawnPointsArray;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Spawn", spawnTime, spawnRepeatRate);
+    }
+
+   void Spawn()
+    {
+        int spawnPointIndex = Random.Range(0, spawnPointsArray.Length);
+        Instantiate(zombie, spawnPointsArray[spawnPointIndex].position, spawnPointsArray[spawnPointIndex].rotation);
+
+    }
+    
+    
+}
