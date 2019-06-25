baacfc440e642ca6b80d0f2873554b4a04558f98
James <50862535+JamesBridger@users.noreply.github.com>
2019-06-25 21:19:42 +0800

    Shooting
    Updated shooting script
    -Firing effect for all the guns
    -Firing effect for the shotgun
    -Added camera shake to Shotgun, Semi Auto & High Power Rifle

--- Zombie Project/Assets/_Scripts/Player/Shooting.cs
+++ Zombie Project/Assets/_Scripts/Player/Shooting.cs
@@ -12,8 +12,6 @@ public class Shooting : MonoBehaviour
 {
     GameObject gameManager;
 
-    public float timer = 0.1f;                   // count down timer for fireRate
-    public float fireRate = 0f;                  // Time between shots set value for each bullet
     public float reloadTime = 2f;                // Time to reload
     public float curReloadTime;                  // current time in reload
     public float ammoCount;                      // Count of bullets in gun 
@@ -25,64 +23,66 @@ public class Shooting : MonoBehaviour
     public int fireMode;                         // Full Auto = 1, Single Shot = 2, Burst fire = 3
 
     public bool isReloading;                     // Player has pushed R
-    public bool isShooting = true;               // Player has pushed Fire1
+    public bool isShooting = false;              // Player has pushed Fire1
     public bool isShotGun = false;               // Player is using shotgun
-    //public bool isSemiAuto = false;            // Done in Fire Mode
-    //public bool isFullAuto = false;            // Done in Fire Mode
 
-    public GameObject reloadReminder;            // reload text object "reload" reminder for player
     public Transform[] bulletTransform;          // bullet spawn for projectile and fireFX
     public GameObject bulletPrefab;              // Projectile prefab
     public AudioSource fireSFX;                  // Sound effects for firing
     public GameObject fireFx;                    // Particle effect when firing
-    public GameObject shotGunFX;                 // Particle effect for shotgun firing
+
     //public AudioSource reloadSFX;              // Reload sound efffect
 
     public void Start()
     {
         ammoReload = 2147483647;                 // MaxValue for Integer
+
         gameManager = GameObject.FindGameObjectWithTag("GameController");
+    }
+
+    public void OnEnable()
+    {
+        isShooting = false;
     }
   
     private void Update()
     {
-
-        timer -= Time.deltaTime;
-
         if (fireMode == 1) // FullAuto = High Power Rifle
         {
             if (Input.GetButton("Fire1") && !isReloading && curAmmo > 0)
             {
-                if (isShooting)
+                if (!isShooting)
                 {
                     curAmmo--;
-                    isShooting = false;
+                    isShooting = true;
                     Shoot();
                     StartCoroutine(ShootingPewPew());
-                    Camera.main.GetComponent<ScreenShake>().CamShake(.9f, .2f);
+
+                    Camera.main.GetComponent<ScreenShake>().CamShake(.7f, .2f); //Camera shake when firing
                 }
             }
         }
-        else if (fireMode == 2) // Single Fire = Handgun, .22 Rifle and Shotgun
+        else if (fireMode == 2) //Single Fire = Handgun, .22 Rifle and Shotgun
         {
             if (Input.GetButtonDown("Fire1") && !isReloading && curAmmo > 0)
             {
-                if (isShooting)
+                if (!isShooting) //check not shooting already
                 {
                     curAmmo--;
-                    isShooting = false;
+                    isShooting = true;  //set shoot flag so we cant shoot
                     Shoot();
                     StartCoroutine(ShootingPewPew());
                 }
             }
         }
-        else if (fireMode == 3) // Burst Fire = Semi Auto Rifle
+        else if (fireMode == 3) //Burst Fire = Semi Auto Rifle
         {
             if (Input.GetButtonDown("Fire1") && !isReloading && curAmmo > 0 && ammoCount == 0)
             {
                 isShooting = true;
                 BurstFire();
-                Camera.main.GetComponent<ScreenShake>().CamShake(.8f, .1f);
+
+                Camera.main.GetComponent<ScreenShake>().CamShake(.8f, .1f); //Camera shake when firing
             }
         }
 
@@ -95,7 +95,7 @@ public class Shooting : MonoBehaviour
 
         if (isReloading)
         {
-            curReloadTime -= Time.fixedDeltaTime;
+            curReloadTime -= Time.deltaTime;
 
             if (curReloadTime <= 0)
             {
@@ -154,7 +154,7 @@ public class Shooting : MonoBehaviour
 
         if (fireMode != 3)
         {
-            isShooting = true;
+            isShooting = false; // reset shot flag so we can shoot again
         }
 
         if (fireMode == 3)
@@ -166,28 +166,21 @@ public class Shooting : MonoBehaviour
 
     void Shoot()
     {
-        if (timer > 0)
-        {
-            return;
-        }
-
         if (fireSFX != null)
         {
             fireSFX.Play();
         }
 
-        Instantiate(bulletPrefab, bulletTransform[0].position, bulletTransform[0].transform.rotation);
-        GameObject.Instantiate(fireFx, bulletTransform[0].position, bulletTransform[0].rotation);
+         Instantiate(bulletPrefab, bulletTransform[0].position, bulletTransform[0].transform.rotation);
+
+         Instantiate(fireFx, bulletTransform[0].position, bulletTransform[0].rotation);
 
         if (isShotGun)
         {
             Instantiate(bulletPrefab, bulletTransform[1].position, bulletTransform[1].transform.rotation);
             Instantiate(bulletPrefab, bulletTransform[2].position, bulletTransform[2].transform.rotation);
-            //GameObject.Instantiate(fireFx, bulletTransform[1].position, bulletTransform[1].rotation);
-            GameObject.Instantiate(shotGunFX, bulletTransform[2].position, bulletTransform[2].rotation);
-            Camera.main.GetComponent<ScreenShake>().CamShake(1.0f, .2f);
-        }
 
-        timer = fireRate;
+            Camera.main.GetComponent<ScreenShake>().CamShake(2.0f, .3f); //Camera shake when firing
+        }
     }
 }
