c0037420cc15422ec76a10c67d57156bfb1908ea
DanBeaumont <daniel.beaumont@aie.edu.au>
2019-07-09 21:26:45 +0930

    Further fixes to visibility script

--- Zombie Project/Assets/_Scripts/Other/VisibilityBehindObject.cs
+++ Zombie Project/Assets/_Scripts/Other/VisibilityBehindObject.cs
@@ -17,16 +17,18 @@ public class VisibilityBehindObject : MonoBehaviour
     private void Update()
     {
         float halfSize = rend.bounds.size.x / 2;
-        float objectHeight = rend.bounds.size.y;
+        float objectHeight = rend.bounds.size.y/2;
+        float yBuffer = 0.1f;
 
-        Vector3 centerOfObject = new Vector3(transform.position.x + halfSize, transform.position.y, transform.position.z);
-        Vector3 farEdgeOfObject = new Vector3(transform.position.x + rend.bounds.size.x, transform.position.y, transform.position.z);
+        Vector3 closeEdgeOfObject = new Vector3(transform.position.x, transform.position.y + yBuffer, transform.position.z - rend.bounds.size.z);
+        Vector3 centerOfObject = new Vector3(transform.position.x + halfSize, transform.position.y + yBuffer, transform.position.z - rend.bounds.size.z);
+        Vector3 farEdgeOfObject = new Vector3(transform.position.x + rend.bounds.size.x, transform.position.y + yBuffer, transform.position.z - rend.bounds.size.z);
 
-        if(Physics.Raycast(transform.position, -transform.forward, objectHeight) ||
+        if(Physics.Raycast(closeEdgeOfObject, -transform.forward, objectHeight) ||
            Physics.Raycast(centerOfObject, -transform.forward, objectHeight) ||
            Physics.Raycast(farEdgeOfObject, -transform.forward, objectHeight))
         {
-            Debug.DrawRay(transform.position, -transform.forward * objectHeight, Color.red);
+            Debug.DrawRay(closeEdgeOfObject, -transform.forward * objectHeight, Color.red);
             Debug.DrawRay(centerOfObject, -transform.forward * objectHeight, Color.red);
             Debug.DrawRay(farEdgeOfObject, -transform.forward * objectHeight, Color.red);
 
